cd2d9ed7d6bff068733684c84d8ddd679d75df5a
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-06-02 23:19:39 +0500

    sir aneeq assignment

---
+++ 01_uv/01_with_package/hello/src/hello/__init__.py
@@ -0,0 +1,3 @@
+def main() -> None:
+    print("Hello from Naila Adnan!")
+    print("UV run with package!")
---
+++ 01_uv/02_without_package/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from Naila Adnan!")
+    print("UV run without package!")
+
+
+if __name__ == "__main__":
+    main()
